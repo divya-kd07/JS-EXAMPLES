f9ef6484c98d2739a2de420594ad8a70011c0288
divya-kd07 <82588428+divya-kd07@users.noreply.github.com>
2021-04-20 21:21:40 -0700

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,9 @@
+let cabType="micro";
+let price=0;
+switch(cabType){
+    case"micro":price=10;break;
+    case"mini":price=10;break;
+    case"price":price=10;break;
+    default:price=10;
+}
+console.log(price);
