90efd353a266ca989c8aea742db53612ad3c4a5c
divya-kd07 <82588428+divya-kd07@users.noreply.github.com>
2021-04-20 21:21:24 -0700

    Add files via upload

---
+++ string.js
@@ -0,0 +1,11 @@
+let username="student";
+let password="pass345";
+let length=password.length;
+if(username=="admin"&& password.length>=8){
+    console.log("validuser");
+}
+else{
+    console.log("validuser");
+}
+console.log(username.toUpperCase());
+console.log(password.toLowerCase());
